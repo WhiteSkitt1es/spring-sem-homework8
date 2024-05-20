16aad80cdd440b2c7273a72f3c12a0526d605ac7
white_skittles <pasha.artemev48@gmail.com>
2024-05-20 19:36:20 +0300

    Task 8 completed!

---
+++ src/main/java/ru/app/SpringSemHomework8Application.java
@@ -0,0 +1,13 @@
+package ru.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSemHomework8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSemHomework8Application.class, args);
+    }
+
+}
